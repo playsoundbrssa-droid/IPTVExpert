abbfc441fabf71acffa1c0cef1ce00397cba9698
playsoundbrssa <playsoundbrssa@gmail.com>
2026-04-19 18:00:45 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: { port: 5173, host: true },
-    build: {
-        chunkSizeWarningLimit: 1600,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    }
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'zustand'],
+          supabase: ['@supabase/supabase-js'],
+        },
+      },
+    },
+  },
 });
